774044c347d9d10a6a26f6aa19e1432aaf822002
Chanuka Herath <malanherath4@gmail.com>
2026-03-21 00:36:37 +0530

    fixed some issues at admin dashboard menu display

--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -9,7 +9,8 @@ const Input = ({
   icon,
   ...props
 }) => {
-  const Icon = icon;
+  const iconIsElement = React.isValidElement(icon);
+  const Icon = !iconIsElement && typeof icon === 'function' ? icon : null;
   const inputClasses = `
     block w-full ${Icon ? 'pl-10 pr-3' : 'px-3'} py-2 border rounded-md shadow-sm placeholder-gray-400 
     focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500
@@ -25,9 +26,9 @@ const Input = ({
         </label>
       )}
       <div className="relative">
-        {Icon && (
+        {(iconIsElement || Icon) && (
           <span className="absolute inset-y-0 left-0 pl-3 flex items-center text-gray-400">
-            <Icon className="w-4 h-4" />
+            {iconIsElement ? icon : <Icon className="w-4 h-4" />}
           </span>
         )}
         <input
